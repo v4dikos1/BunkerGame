a475f44363cdd02dcc4b071e16529ae5eb37797d
vadim <v4dikos@yandex.ru>
2021-06-08 19:41:36 +0700

    Начальные настройки

--- app/src/main/java/com/example/bunker/StartActivity.kt
+++ app/src/main/java/com/example/bunker/StartActivity.kt
@@ -3,9 +3,9 @@ package com.example.bunker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_start)
     }
 }
